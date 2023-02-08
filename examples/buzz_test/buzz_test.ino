c57ecc3cd9f7580c8643ca550e25ae8d348fb762
misaproyectil <40368863+misaproyectil@users.noreply.github.com>
2023-02-08 10:10:52 -0600

    feat: Update examples

--- examples/buzz_test/buzz_test.ino
+++ examples/buzz_test/buzz_test.ino
@@ -1,15 +1,33 @@
 #include "digitalIO.h"
 
 digitalIO :: Buzzer b;
+#define BUZZER 6
 void setup() {
   // put your setup code here, to run once:
- 
- b.buzzerBegin(A2);
- b.playSound(RIGHT);
- delay(1000);
- b.melody(MELODY);
- delay(1000);
- b.bip();
+
+  b.buzzerBegin(BUZZER);// inicializa el pin para el zumbador
+
+
+  b.playSound(RIGHT);
+  delay(2000);
+  b.playSound(WINS);
+  delay(2000);
+  b.playSound(WRONG);
+  delay(2000);
+  b.playSound(BIP);
+  delay(2000);
+  b.playSound(BIP2);
+  delay(2000);
+
+  b.melody();
+  delay(2000);
+  b.melody(WINS);
+  delay(1000);
+  b.melody(WRONG);
+  delay(1000);
+  b.bip();
+  delay(1000);
+
 }
 
 void loop() {
---
+++ examples/digitalIO_test/digitalIO_test.ino
@@ -0,0 +1,56 @@
+/*
+   Biblioteca digitalIO by Proyectil Mx
+
+   Ejemplo de uso para entradas y salidas digitales
+
+
+   shield Proyectil
+
+*/
+#include <digitalIO.h>
+
+digitalIO input;
+digitalIO btn(A1);
+digitalIO led1(9);
+digitalIO led2;
+digitalIO rele(A0);
+
+int number;
+void setup() {
+  Serial.begin(115200);
+
+  input.begin(3, INPUT_PULLUP);
+  btn.begin(INPUT_PULLUP);
+  led1.begin(OUTPUT);
+  led2.begin(10, OUTPUT);
+  rele.begin(OUTPUT);
+
+ 
+  Serial.println("Inicia digitalIo test..");
+}
+
+void loop() {
+
+  led1.setOutput(btn.getState());
+
+  if (btn.getTimeOn() > 2000)
+  {
+    led2.blink(1, 100);
+  }
+
+  if (input.getTimeOn() > 5000)
+  {
+    rele.setOutput(!rele.state);
+    while(!input.getState());
+  }
+
+  number = input.getNumber(); 
+  if (number != 0)
+  {
+    Serial.println(number);
+    led1.blink(number, 500);
+    
+   
+  }
+
+}
